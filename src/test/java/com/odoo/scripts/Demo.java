b5d7d0d6b4db3129e3031fc783aab4691b654055
Digi27 <diganta.sdp@gmail.com>
2019-10-04 15:11:47 +0530

    reports, demo suite file, modified gitignore

---
+++ src/test/java/com/odoo/scripts/Demo.java
@@ -0,0 +1,16 @@
+package com.odoo.scripts;
+
+import org.testng.annotations.Test;
+
+import com.odoo.webutils.BaseAbstractTest;
+
+public class Demo extends BaseAbstractTest
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("demo script");
+	}
+	
+	
+}
